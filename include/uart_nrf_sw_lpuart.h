0ec650dc1e5923f2612c639f44f51fb06d0184c1
td-mlokcewicz <michal.lokcewicz@jrdltd.co.uk>
2025-10-01 14:19:50 +0200

    Add rx buf reset function to allow reinit

--- include/uart_nrf_sw_lpuart.h
+++ include/uart_nrf_sw_lpuart.h
@@ -5,4 +5,8 @@
 
 int uart_nrf_sw_lpuart_init(const struct device *dev);
 
+/*  Allows to reset RX buffer pointer and prepare to reinitialize lpuart.
+    Should be called only when lpuart instance is disabled */
+void uart_nrf_sw_lpuart_rx_buf_reset(const struct device *dev);
+
 #endif // UART_NRF_SW_LPUART_H_
